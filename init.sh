f1a97fe565df24d09e4cee703e054c98d7daaf79
Maryo <maryo@iMac.modem>
2025-02-06 18:34:49 +1000

    Completed v1 Bash script

--- init.sh
+++ init.sh
@@ -1,24 +1,49 @@
 #!/bin/bash
 
+text_log() { echo "### $1"; }
+print_done_and_exit() { printf "\n~ DONE ~\n\n"; exit; }
+
 clear;
-: '
+# read bash parameters
+: ' =======================================================
+Local dev
     Run --> sh init.sh
     to initialise the project
+
+===========================================================
+Remote BASH script
+    -f
+    --force
+
+    v15
+===========================================================
 '
 
-text_log() { echo "\n ### $1 "; }
+branch="v15"
+
+for p in $@; do
+    if [[ $p == *"v"* ]]; then
+        branch=$p
+    fi
+done
+
+repo="BA-Creative/Roxify"
+repo_ssh="git@github.com:BA-Creative/Roxify.git"
+repo_zip="https://github.com/BA-Creative/Roxify/tar.gz/$branch"
+repo_url="https://raw.githubusercontent.com/BA-Creative/Roxify"
 
 printf "======================================================== \n";
 printf "Please enter a Shopify Store handle \n";
 printf "Example: \"online-store\" for online-store.myshopify.com \n\n";
 read -p "--> " myshopify;
-printf "\n\n========================================================\n\n";
+printf "\n========================================================\n\n";
 
 root="$PWD/$myshopify"
 
 if [ -d $root ]; then
-    echo "\n!!! Folder already exists. Please DELETE or RENAME the existing folder, then try again. \n";
-    exit;
+    printf "!!! Folder already exists. Would you like to OVERWRITE ? \n";
+    read -p "(y/n): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || print_done_and_exit;
+    rm -rf $root;
 fi
 
 # Create directory
@@ -27,28 +52,11 @@ mkdir -p $root;
 # Clone lastest version of Dawn
 git clone "https://github.com/Shopify/dawn.git" $root;
 
-# read bash parameters
-: '
-    -f
-    --force
+# Remove .git folder
+rm -rf "$root/.git";
 
-    v15
-'
-__force=false;
-base_url="https://raw.githubusercontent.com/BA-Creative/Roxify"
-branch="v15"
+# Clone Roxify
+curl -L "https://api.github.com/repos/$repo/tarball/$branch" | tar xzf - --strip-components=1 -C $root;
 
-for p in $@; do
-    if [ $p == "--force" ] || [ $p == "-f" ]; then
-        __force=true
-    fi
-    if [[ $p == *"v"* ]]; then
-        branch=$p
-    fi
-done
-
-files=(".shopifyignore")
-for file in "${files[@]}"
-do
-    curl -s "$base_url/$branch/$file" -o "$root/$file"
-done
+# Done
+print_done_and_exit;
